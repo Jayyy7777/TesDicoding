07bb703c44506b54d880939e4b4ed0a14262370f
Jayyy7777 <gianakhiruramadhan14@gmail.com>
2023-10-03 08:43:40 +0700

    tes

--- app/src/main/java/com/example/mobileprogram/MainActivity.kt
+++ app/src/main/java/com/example/mobileprogram/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.mobileprogram
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -9,69 +10,21 @@ import android.widget.TextView
 import com.example.mobileprogram.R
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
-    private lateinit var edtWidth: EditText
-    private lateinit var edtHeight: EditText
-    private lateinit var edtLength: EditText
-    private lateinit var btnCalculate: Button
-    private lateinit var tvResult: TextView
+    private lateinit var btn_switch: Button
 
-    companion object {
-        private const val STATE_RESULT = "state_result"
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        edtWidth = findViewById(R.id.edt_width)
-        edtHeight = findViewById(R.id.edt_height)
-        edtLength = findViewById(R.id.edt_length)
-        btnCalculate = findViewById(R.id.btn_calculate)
-        tvResult = findViewById(R.id.tv_result)
-
-        btnCalculate.setOnClickListener(this)
-
-        if (savedInstanceState != null) {
-            val result = savedInstanceState.getString(STATE_RESULT)
-            tvResult.text = result
+        btn_switch = findViewById(R.id.btn_switch)
+        btn_switch.setOnClickListener(this)
         }
-    }
 
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        outState.putString(STATE_RESULT, tvResult.text.toString())
-    }
-
-    override fun onClick(view: View?) {
-        if (view?.id == R.id.btn_calculate) {
-            val inputLength = edtLength.text.toString().trim()
-            val inputWidth = edtWidth.text.toString().trim()
-            val inputHeight = edtHeight.text.toString().trim()
-
-            var isEmptyFields = false
-
-            /*
-            Validasi apakah inputan masih ada yang kosong
-             */
-            if (inputLength.isEmpty()) {
-                isEmptyFields = true
-                edtLength.error = "Field ini tidak boleh kosong"
-            }
-            if (inputWidth.isEmpty()) {
-                isEmptyFields = true
-                edtWidth.error = "Field ini tidak boleh kosong"
-            }
-            if (inputHeight.isEmpty()) {
-                isEmptyFields = true
-                edtHeight.error = "Field ini tidak boleh kosong"
-            }
-
-            /*
-            Jika semua inputan valid maka tampilkan hasilnya
-             */
-            if (!isEmptyFields) {
-                val volume = inputLength.toDouble() * inputWidth.toDouble() * inputHeight.toDouble()
-                tvResult.text = volume.toString()
+    override fun onClick(v: View?) {
+        when(v?.id) {
+            R.id.btn_switch -> {
+                val move = Intent (this@MainActivity, activity_move::class.java)
+                startActivity(move)
             }
         }
     }
